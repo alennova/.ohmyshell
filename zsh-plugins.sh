8a80a542d55009e1056889ced0f8456865d55bcf
alennova <alennova@gmail.com>
2025-02-10 17:12:01 +0300

    Auto commit - 2025-02-10 17:12:00

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Clone the plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &&
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting &&
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 
-echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting)" >> $HOME/.ohmyshell/zshrc &&
+echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting)" >> $HOME/.ohmyshell/zshrc
 
 echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting) added in $HOME/.ohmyshell/zshrc file"
