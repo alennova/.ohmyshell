3e2a350c780b8bef4eec52ca1d217a5d602d8ff2
alennova <alennova@gmail.com>
2025-02-10 17:10:44 +0300

    Auto commit - 2025-02-10 17:10:44

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -5,8 +5,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting &&
 
-echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting)" >> $HOME/.ohmyshell/zshrc
+echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting)" >> $HOME/.ohmyshell/zshrc &&
 
 echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting) added in $HOME/.ohmyshell/zshrc file"
-
-$/.oh-my-zsh/custom/plugins/
